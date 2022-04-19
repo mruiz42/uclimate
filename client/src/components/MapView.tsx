b461337aa431ca2e034b2d7e7a6270a50855bec9
Michael Ruiz <michael@mruiz.dev>
2022-04-19 16:53:07 -0700

    changed default zoom and coordinates

--- client/src/components/MapView.tsx
+++ client/src/components/MapView.tsx
@@ -6,34 +6,37 @@ const containerStyle = {
   height: '400px'
 };
 
+const zoom = 5;
+
 const center = {
-  lat: 37,
-  lng: -121
+  lat: 36,
+  lng: -119
 };
+
 // https://react-google-maps-api-docs.netlify.app/#directionsservice
 const MapView = (props: any) => {
   const {apiKey, map, setMap, markers, formData, directions} = props;
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: apiKey
-  })
+  });
 
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds();
     map.fitBounds(bounds);
-    setMap(map)
-  }, [])
+    setMap(map);
+  }, []);
 
   const onUnmount = React.useCallback(function callback(map) {
     setMap(null);
-  }, [])
+  }, []);
 
 
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
-      zoom={10}
+      zoom={zoom}
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
@@ -44,4 +47,4 @@ const MapView = (props: any) => {
   ) : <></>
 }
 
-export default React.memo(MapView)
+export default React.memo(MapView);
